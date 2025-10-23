777ac3ef75560c1907eb36c3aa56366973c4d1b2
charan lingolu <charanlingolu@gmail.com>
2025-10-23 14:33:14 +0530

    first-commits

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://studentmarktracker-backend-873w.onrender.com",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
